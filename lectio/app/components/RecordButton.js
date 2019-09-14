e6dbe16205a01e0569a9bdf5f23fe1955f05c2da
Justin Kavalan <justinkavalan@gmail.com>
2019-09-14 19:20:00 -0400

    Add button to record

---
+++ lectio/app/components/RecordButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, Button } from 'react-native'
+
+const RecordButton = (props) => {
+    return (
+        <Button title="Record"></Button>
+    )
+}
+
+export default RecordButton;
